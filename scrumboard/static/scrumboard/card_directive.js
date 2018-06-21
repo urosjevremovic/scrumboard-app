737df76f129fc9bd255a7451e0bbacbafb058883
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-06-21 20:59:56 +0200

    added front end for updating and deleting scrumboards, and added css

--- scrumboard/static/scrumboard/card_directive.js
+++ scrumboard/static/scrumboard/card_directive.js
@@ -6,7 +6,34 @@
     function CardDirective() {
         return {
             templateUrl: 'static/scrumboard/card.html',
-            restrict: 'E'
+            restrict: 'E',
+            controller: ['$scope', '$http', function ($scope, $http) {
+                var url ='/scrumboard/cards/' + $scope.card.id + '/';
+                $scope.update = function () {
+                    $http.put(
+                        url,
+                        $scope.card
+                    );
+                };
+
+                $scope.delete = function(){
+                    $http.delete(url).then(
+                        function () {
+                            var cards = $scope.list.cards;
+                            card.splice(
+                                cards.indexOf($scope.card),
+                                1
+                            );
+                            
+                        }
+                    );
+                };
+
+                $scope.modelOptions = {
+                    debounce: 500
+                };
+                
+            }]
         };
         
     }
